14ea9a76cbf1bae8a0600576652e844e5385d92d
Jan <milesan.eugen@gmail.com>
2025-06-01 17:48:24 +0300

    update

--- stiri.js
+++ stiri.js
@@ -18,12 +18,6 @@ const stiri = [
       rezumat: "Oferă medicină generală, stomatologie și analize gratuite pentru pensionari...",
       continut: "Centrul funcționează de luni până vineri între orele 8-18 și este dotat cu aparatură modernă. Este primul centru care oferă analize gratuite pentru toți pensionarii din județ, pe baza programării."
     },
-    {
-      id: 4,
-      titlu: "Cod Galben in Bistrita-Nasaud",
-      rezumat: "Administrația Națională de Meteorologie a emis o avertizare cod galben de vreme rea, valabil și pentru județul Bistrița-Năsăud duminică 1 iunie.",
-      continut: "În Carpații Orientali, estul Carpaților Meridionali și local în Munții Apuseni, Maramureș, estul Transilvaniei, nord-vestul Moldovei și nordul Munteniei instabilitatea atmosferică se va accentua și se va manifesta prin averse torențiale, descărcări electrice, intensificări de scurtă durată ale vântului (rafale în general de 50…70 km/h) și pe alocuri vijelii și căderi de grindină. Cantitățile de apă vor fi de 15…25 l/mp și izolat de 30…40 l/mp. Prognoza pentru Transilvania în săptămâna 2-8 iunie: Începând din 2 iunie, valorile termice vor intra pe o pantă ascendentă, astfel încât până la finalul săptămânii se vor înregistra temperaturi mai apropiate de mediile climatologice. Maximele vor ajunge, în medie, la 22…27 de grade, iar minimele vor fi de 10…13 grade. Pot fi intervale cu averse."
-    },
   ];
   const main = document.getElementById("main-content");
   
